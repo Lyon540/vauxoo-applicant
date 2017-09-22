ffaef333057ece63345c7b6ca4f59007834585e0
Lyon540 <lmgc183@gmail.com>
2017-09-22 17:40:47 -0500

    Update employee_vauxoo.sql

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -4,17 +4,17 @@
 --       Consider add ';' at end sentence.
 
 CREATE TABLE employee (
-Id_empleado	Int,
-first_name  Varchar(50),
-last_name	varchar(80)
+Id_empleado		Int,
+first_name  	Varchar(50),
+last_name		varchar(80)
 );
 
 Insert into employee values(1,'Luis','Guerrero');
 Insert into employee values(2,'Tania','Gutierrez');
 Insert into employee values(3,'Rocio','Solis');
 Insert into employee values(4,'Guadalupe','Puente');
-
-Create table employee_department(
+*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+CREATE TABLE employee_department (
 Id_dept		Int Primary key,
 name_dept	Varchar(50),
 Descripcion Varchar(50)
@@ -26,13 +26,12 @@ Insert into employee_department values(10003,'Produccion', 'Se producen los arti
 Insert into employee_department values(10004,'Soporte','Da ayuda a equipos de computo en general');
 Insert into employee_department values(10005,'Sistemas','Atiende reportes y soluciona problemas');
 Insert into employee_department values(10006,'Desarrollo','Realiza modificaciones al programa');
-
+*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 Create table Emp_dept(
 Id_empleado	Int,
 Id_dept		int,
 name_dept	Varchar(50)
 );
-
 insert into Emp_dept values (2,10002,'Ventas');
 insert into Emp_dept values (1,10006,'Desarrollo');
 insert into Emp_dept values (3,10004,'Soporte');
@@ -41,9 +40,19 @@ insert into Emp_dept values (4,10003,'Produccion');
 Select Emp_dept.Id_empleado, Emp_dept.name_dept, employee.first_name, employee.last_name  from Emp_dept
 Inner join employee
 On Emp_dept.Id_empleado=employee.Id_empleado;
-
-CREATE TABLE employee_department (
+*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+CREATE TABLE employee_hobby (
+  Id_hobby		int,
+name_hobby		Varchar (50),
+Descripcion		Varchar (50),
 );
+
+Insert into employee_hobby values(1, 'Guitarra', 'Tocar Guitarra');
+Insert into employee_hobby values(2, 'Videojuegos','Jugar en consolas y/o pc');
+Insert into employee_hobby values(3, 'Peliculas','Ver peliculas en casa o en cine');
+Insert into employee_hobby values(4, 'Series', 'Ver series en Netflix');
+
+*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
 CREATE TABLE employee_hobby (
 );
