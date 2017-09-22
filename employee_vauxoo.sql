896456a7dc32b09e1ae5b0ea85aa067daa02a2c2
Lyon540 <lmgc183@gmail.com>
2017-09-22 15:58:38 -0500

    Update employee_vauxoo.sql

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -4,7 +4,42 @@
 --       Consider add ';' at end sentence.
 
 CREATE TABLE employee (
+Id_empleado	Int Primary key,
+first_name  Varchar(50),
+last_name	varchar(80)
 );
+
+Insert into employee values(1,'Luis','Guerrero');
+Insert into employee values(2,'Tania','Gutierrez');
+Insert into employee values(3,'Rocio','Solis');
+Insert into employee values(4,'Guadalupe','Puente');
+
+Create table employee_department(
+Id_dept		Int Primary key,
+name_dept	Varchar(50),
+Descripcion Varchar(50)
+);
+
+Insert into employee_department values(10001,'Finanzas','Controla la sitacion financiera de la empresa');
+Insert into employee_department values(10002,'Ventas','Se lleva control de los productos vendidos ');
+Insert into employee_department values(10003,'Produccion', 'Se producen los articulos a vender'):
+Insert into employee_department values(10004,'Soporte','Da ayuda a equipos de computo en general');
+Insert into employee_department values(10005,'Sistemas','Atiende reportes y soluciona problemas');
+Insert into employee_department values(10006,'Desarrollo','Realiza modificaciones al programa');
+
+Create table Emp_dept(
+Id_empleado	Int,
+Id_dept		Int,
+);
+
+insert into Emp_dept values (2,10002);
+insert into Emp_dept values (1,10006);
+insert into Emp_dept values (3,10004);
+insert into Emp_dept values (4,10003);
+
+Select Emp_dept.Id_empleado, Emp_dept.Id_dept, employee.first_name, employee.last_name  from Emp_dept
+Inner join employee
+On Emp_dept.Id_empleado=employee.Id_empleado;
 
 CREATE TABLE employee_department (
 );
